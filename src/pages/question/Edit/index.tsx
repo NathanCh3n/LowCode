e0df27c4f194216dfe54318ee7be35caab3d2720
Captain Drake <9393079+captain_drake@user.noreply.gitee.com>
2025-02-22 21:26:17 +0800

    feature: 画布Y轴滚动

--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -9,12 +9,14 @@ const Edit: FC = () => {
 
   return (
     <div className={styles.container}>
-      <div style={{ backgroundColor: '#fff' }}>Header</div>
+      <div style={{ backgroundColor: '#fff', height: '40px' }}>Header</div>
       <div className={styles['content-wrapper']}>
         <div className={styles.content}>
           <div className={styles.left}>Left</div>
           <div className={styles.main}>
-            <div className={styles['canvas-wrapper']}>画布</div>
+            <div className={styles['canvas-wrapper']}>
+              <div style={{ height: '900px' }}>画布滚动测试</div>
+            </div>
           </div>
           <div className={styles.right}>Right</div>
         </div>
