880bdaf293cb6336e7ef6f86b1a10d807e9c78bf
Captain Drake <9393079+captain_drake@user.noreply.gitee.com>
2025-02-24 23:21:18 +0800

    feat: 画布动态显示组件列表

--- src/pages/question/Edit/EditCanvas.tsx
+++ src/pages/question/Edit/EditCanvas.tsx
@@ -1,17 +1,28 @@
 import React, { FC } from 'react'
 import styles from './EditCanvas.module.scss'
-import QuestionTitle from '../../../component/QuestionComponents/QuestionTitle/Component'
-import QuestionInput from '../../../component/QuestionComponents/QuestionInput/Component'
+// import QuestionTitle from '../../../component/QuestionComponents/QuestionTitle/Component'
+// import QuestionInput from '../../../component/QuestionComponents/QuestionInput/Component'
 import { Spin } from 'antd'
 import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
+import { getComponentConfByType } from '../../../component/QuestionComponents'
+import { ComponentInfoType } from '../../../store/componentReducer'
 
 type PropsType = {
   loading: boolean
 }
 
+function getComponent(componentInfo: ComponentInfoType) {
+  const { type, props } = componentInfo
+  const componentConf = getComponentConfByType(type)
+  if (!componentConf) {
+    return null
+  }
+  const { Component } = componentConf
+  return <Component {...props} />
+}
+
 const EditCanvas: FC<PropsType> = ({ loading }) => {
   const { componentList } = useGetComponentInfo()
-  console.log('componentList', componentList)
   if (loading) {
     return (
       <div style={{ textAlign: 'center', padding: '20px' }}>
@@ -21,18 +32,29 @@ const EditCanvas: FC<PropsType> = ({ loading }) => {
   }
   return (
     <div className={styles.canvas}>
-      <div className={styles['component-wrapper']}>
-        <div className={styles.component}>
-          <QuestionTitle />
-        </div>
-      </div>
-      <div className={styles['component-wrapper']}>
-        <div className={styles.component}>
-          <QuestionInput />
-        </div>
-      </div>
+      {componentList.map(c => {
+        const { fe_id } = c
+        return (
+          <div key={fe_id} className={styles['component-wrapper']}>
+            <div className={styles.component}>{getComponent(c)}</div>
+          </div>
+        )
+      })}
     </div>
   )
+
+  // <div className={styles.canvas}>
+  //   <div className={styles['component-wrapper']}>
+  //     <div className={styles.component}>
+  //       <QuestionTitle />
+  //     </div>
+  //   </div>
+  //   <div className={styles['component-wrapper']}>
+  //     <div className={styles.component}>
+  //       <QuestionInput />
+  //     </div>
+  //   </div>
+  // </div>
 }
 
 export default EditCanvas
